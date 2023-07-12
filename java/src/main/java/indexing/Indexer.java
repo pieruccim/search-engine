f9c45409be98cd79916c4b189dc42c3de4006862
Matteo Pierucci <matteo.pierucci4@gmail.com>
2023-07-12 19:05:50 +0200

    Added methods processCorpus() and addToIndex()

--- java/src/main/java/indexing/Indexer.java
+++ java/src/main/java/indexing/Indexer.java
@@ -3,6 +3,11 @@ package indexing;
 import common.bean.DocumentIndex;
 import common.bean.InvertedIndex;
 import common.bean.Vocabulary;
+import javafx.util.Pair;
+import preprocessing.Preprocessor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Indexer {
 
@@ -11,9 +16,62 @@ public class Indexer {
     private InvertedIndex invertedIndex;
 
     public Indexer(){
-        vocabulary = new Vocabulary();
-        documentIndex = new DocumentIndex();
-        invertedIndex = new InvertedIndex();
+        this.vocabulary = new Vocabulary();
+        this.documentIndex = new DocumentIndex();
+        this.invertedIndex = new InvertedIndex();
+    }
+
+    private void addToIndex(String docText, int docNo){
+        // TODO: Manage saving of the Block to disk when memory is almost empty (SPiMI)
+
+        String[] terms = Preprocessor.processText(docText, false);
+
+        // Count term frequencies in the document
+        HashMap<String, Integer> termCounter = new HashMap<>();
+        for (String term: terms) {
+            termCounter.put(term, termCounter.containsKey(term) ? termCounter.get(term)+1 : 1);
+        }
+
+        /**System.out.println("Docu: " + docNo);
+
+        for (Map.Entry<String, Integer> entry : termCounter.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("term: " + key + ", counter: " + value);
+        }*/
+
+        // TODO: Update Vocabulary, Posting List, ecc...
+
+        invertedIndex.addPostings(docNo, termCounter);
+    }
+
+    public void processCorpus(){
+
+        // TODO: Call to FileManager to retrieve corpus of documents: now is emulated
+        String[] lines = {  "0\tThe presence of communication amid scientific minds was equally important to the success of the Manhattan Project as scientific intellect was. The only cloud hanging over the impressive achievement of the atomic researchers and engineers is what their success truly meant; hundreds of thousands of innocent lives obliterated.\n",
+                            "1\tThe Manhattan Project and its atomic bomb helped bring an end to World War II. Its legacy of peaceful uses of atomic energy continues to have an impact on history and science.\n",
+                            "2\tEssay on The Manhattan Project - The Manhattan Project The Manhattan Project was to see if making an atomic bomb possible. The success of this project would forever change the world forever making it known that something this powerful can be manmade.",
+                            "3\tThe Manhattan Project was the name for a project conducted during World War II, to develop the first atomic bomb. It refers specifically to the period of the project from 194 Ã¢Â€Â¦ 2-1946 under the control of the U.S. Army Corps of Engineers, under the administration of General Leslie R. Groves.",
+                            "4\tversions of each volume as well as complementary websites. The first websiteÃ¢Â€Â“The Manhattan Project: An Interactive HistoryÃ¢Â€Â“is available on the Office of History and Heritage Resources website, http://www.cfo. doe.gov/me70/history. The Office of History and Heritage Resources and the National Nuclear Security",
+                            "5\tThe Manhattan Project. This once classified photograph features the first atomic bomb Ã¢Â€Â” a weapon that atomic scientists had nicknamed Gadget.. The nuclear age began on July 16, 1945, when it was detonated in the New Mexico desert."};
+
+        for (String line: lines) {
+
+            Pair<Integer, String> lineFormatted = null;
+
+            try {
+                lineFormatted = Preprocessor.parseLine(line);
+            } catch (IllegalArgumentException e){
+                e.printStackTrace();
+            }
+
+            int docNo = lineFormatted.getKey();
+            String docText = lineFormatted.getValue();
+
+            addToIndex(docText, docNo);
+        }
+
+        System.out.print("manhattan: " + invertedIndex.toString("manhattan"));
     }
 
     /*
