87492e888b17fcf088a0c87c7a6024c451b7f79f
Matteo Pierucci <matteo.pierucci4@gmail.com>
2023-07-12 19:04:11 +0200

    Usage example of the Indexer Class

---
+++ java/src/main/java/indexing/UsageExample.java
@@ -0,0 +1,14 @@
+package indexing;
+
+import javafx.util.Pair;
+import preprocessing.Preprocessor;
+
+public class UsageExample {
+
+    public static void main(String[] args) {
+
+        Indexer indexer = new Indexer();
+        indexer.processCorpus();
+
+    }
+}
