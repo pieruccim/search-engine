0f01fbd7a347d0c34b234e1f032c0c7a475ce9a6
Enrico <nello.enrico@gmail.com>
2023-07-12 18:36:37 +0200

    InvertedIndex first commit

--- java/src/main/java/common/bean/InvertedIndex.java
+++ java/src/main/java/common/bean/InvertedIndex.java
@@ -5,38 +5,77 @@ import java.util.HashMap;
 
 public class InvertedIndex {
 
+    private HashMap<String, ArrayList<PostingInformation>> invertedIndex; // <Key:termid, Value:PostingInformation>
+
     private class PostingInformation{
 
         private int docid;
         private int freq;
 
-        /*
-            costruttore
-            metodi get
-            metodi set
-         */
+        public PostingInformation(int docid, int freq){
+            this.docid = docid;
+            this.freq = freq;
+        }
 
+        public int getDocid() {
+            return docid;
+        }
+
+        public void setDocid(int docid) {
+            this.docid = docid;
+        }
+
+        public int getFreq() {
+            return freq;
+        }
+
+        public void setFreq(int freq) {
+            this.freq = freq;
+        }
+
+        @Override
+        public String toString() {
+            return "PostingInformation{" +
+                    "docid=" + this.docid +
+                    ", freq=" + this.freq +
+                    '}';
+        }
     }
 
-    private ArrayList<PostingInformation> invertedIndex;
-
-    /*      VERSIONE CHE UTILIZZA HASHMAP
-
-    private class PostingList{
-
-        private ArrayList<PostingInformation> PostingList;
-
-
-            costruttore
-            metodi get e set
-            metodo per aggiungere elemento alla posting list
-
-
+    public HashMap<String, ArrayList<PostingInformation>> getInvertedIndex() {
+        return invertedIndex;
+    }
+    
+    public void setInvertedIndex(HashMap<String, ArrayList<PostingInformation>> invertedIndex) {
+        this.invertedIndex = invertedIndex;
     }
 
-    public HashMap<String, PostingList> invertedIndex; // <Key:termid, Value:postingList>
+    public void addPostings(int docid, HashMap<String, Integer> termCounter){
+        for (HashMap.Entry<String, Integer> entry : termCounter.entrySet()) {
+            String term = entry.getKey();
+            Integer frequency = entry.getValue();
+            ArrayList<PostingInformation> currentPostingList = invertedIndex.get(term);
+            // case where posting list doesn't exist
+            if(currentPostingList == null){
+                ArrayList<PostingInformation> newPostingList = new ArrayList<>();
+                newPostingList.add(new PostingInformation(docid, frequency));
+                invertedIndex.put(term, newPostingList);
+            }
+            // case where posting list already exists
+            else{
+                invertedIndex.get(term).add(new PostingInformation(docid, frequency));
+            }
+        }
+    }
 
-
-        metodo per aggiungere una postingList all'inverted Index */
+    // useful for debugging
+    public String toString(String term) {
+        ArrayList<PostingInformation> informationArray = invertedIndex.get(term);
+        String result="";
+        for (PostingInformation information:informationArray) {
+            result += information.toString();
+        }
+        return result;
+    }
 
 }
