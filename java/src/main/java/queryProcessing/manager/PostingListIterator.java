31ec230694b1a4119b751acd5bce21704ff26e69
Giacomo Pacini <giacomopacini98@gmail.com>
2023-07-25 17:41:05 +0200

    PostingListIteratorSingleFile

--- java/src/main/java/queryProcessing/manager/PostingListIterator.java
+++ java/src/main/java/queryProcessing/manager/PostingListIterator.java
@@ -1,9 +1,11 @@
 package queryProcessing.manager;
 
+import java.util.Iterator;
+
 import common.bean.Posting;
 import common.bean.VocabularyFileRecord;
 
-public interface PostingListIterator {
+public interface PostingListIterator extends Iterator<Posting>{
 
     /**
      * 
@@ -15,6 +17,16 @@ public interface PostingListIterator {
      * @return returns the next Posting in the iterator if any, else returns null
      */
     public Posting next();
+    /**
+     * 
+     * @return true if the iterator has not reached the end, else returns true
+     */
+    public boolean hasNext();
+    /**
+     * 
+     * @return the current posting or null if no posting were read still
+     */
+    public Posting getCurrentPosting();
     
     /**
      * moves the iterator towards the end and returns the first Posting whose docId is greater or equal to the specified one
@@ -25,7 +37,6 @@ public interface PostingListIterator {
 
     /**
      * 
-     * @return true if the iterator over the posting was closed correctly, else false
      */
-    public boolean closeList();
+    public void closeList();
 }
