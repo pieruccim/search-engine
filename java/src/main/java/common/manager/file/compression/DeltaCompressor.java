59c6120c34ccf45d0e7a59fa76a5a1aa07d527aa
Matteo Pierucci <matteo.pierucci4@gmail.com>
2023-08-02 15:08:03 +0200

    Delta encoding compressor, that uses Variable Byte Encoding

---
+++ java/src/main/java/common/manager/file/compression/DeltaCompressor.java
@@ -0,0 +1,69 @@
+package common.manager.file.compression;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import static java.lang.Math.log;
+
+public class DeltaCompressor implements Compressor{
+
+    private VariableByteCompressor variableByteCompressor = null;
+
+
+    public DeltaCompressor(){
+        variableByteCompressor = new VariableByteCompressor();
+    }
+
+    /**
+     * @param array: integers array to encode with delta encoding
+     * @return an integer array encoded with delta encoding
+     */
+    private static int[] deltaEncoding(int[] array) {
+        int[] deltaEncodedArray = new int[array.length];
+        deltaEncodedArray[0] = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            deltaEncodedArray[i] = array[i] - array[i - 1];
+        }
+
+        return deltaEncodedArray;
+    }
+
+    /**
+     * @param array of delta encoded integer to decode
+     * @return an integer array of decoded integers
+     */
+    private static int[] deltaDecoding(int[] array) {
+        int[] decompressedArray = new int[array.length];
+        decompressedArray[0] = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            decompressedArray[i] = decompressedArray[i - 1] + array[i];
+        }
+
+        return decompressedArray;
+    }
+
+    @Override
+    public byte[] compressIntArray(int[] data) throws IOException {
+
+        // Apply Delta Encoding to the array
+        int[] deltaEncodedArray = deltaEncoding(data);
+
+        // Apply Variable Byte Encoding to the delta encoded array
+        byte[] compressedData = variableByteCompressor.compressIntArray(deltaEncodedArray);
+
+        return compressedData;
+    }
+
+    @Override
+    public int[] decompressIntArray(byte[] compressedData, int length) throws IOException {
+
+        // Perform Variable Byte Decoding on the compressed data
+        int[] deltaEncodedArray = variableByteCompressor.decompressIntArray(compressedData, length);
+
+        // Perform Delta Decoding on the Variable Byte Decoded array
+        int[] decompressedArray = deltaDecoding(deltaEncodedArray);
+
+        return decompressedArray;
+    }
+}
---
+++ java/src/main/java/common/manager/file/compression/DeltaCompressorMain.java
@@ -0,0 +1,37 @@
+package common.manager.file.compression;
+
+import java.io.IOException;
+
+public class DeltaCompressorMain {
+
+    public static String printBits(byte b) {
+        String ret = "";
+        for (int i = 7; i >= 0; i--) {
+            int bit = (b >> i) & 1;
+            ret+=bit;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int[] integers = {128}; // Example array of integers to compress
+
+        DeltaCompressor dc = new DeltaCompressor();
+        byte[] compressedData = dc.compressIntArray(integers);
+
+        System.out.print("Compressed integers: ");
+        // Printing the compressed data as bytes (for demonstration purposes)
+        for (byte b : compressedData) {
+            System.out.print(printBits(b) + " ");
+        }
+        System.out.println();
+
+        int[] decompressedIntegers = dc.decompressIntArray(compressedData, integers.length);
+
+        System.out.print("Decompressed integers: ");
+        // Print the decompressed integers (for demonstration purposes)
+        for (int num : decompressedIntegers) {
+            System.out.print(num + " ");
+        }
+    }
+}
