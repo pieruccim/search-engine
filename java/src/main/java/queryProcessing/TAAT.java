68e8ac9896a1440b2aeb3d09c255bff7020f442a
Enrico <nello.enrico@gmail.com>
2023-08-10 18:11:28 +0200

    Conjunctive query in TAAT algorithm

--- java/src/main/java/queryProcessing/TAAT.java
+++ java/src/main/java/queryProcessing/TAAT.java
@@ -16,6 +16,7 @@ public class TAAT extends DocumentProcessor {
 
         TreeSet<DocumentScore> priorityQueue = new TreeSet<DocumentScore>(((Comparator<DocumentScore>)(DocumentScore::compare)).reversed());
         Map<Integer, Double> documentScores = new HashMap<>();
+        Map<Integer, Integer> documentQueryTermCount = new HashMap<>(); // to check whether a doc contains all query terms
 
         for (VocabularyFileRecord term : queryTerms) {
             PostingListIterator iterator = PostingListIteratorFactory.openIterator(term);
@@ -23,7 +24,11 @@ public class TAAT extends DocumentProcessor {
             while (iterator.hasNext()) {
                 Posting posting = iterator.next();
                 int docId = posting.getDocid();
-                //get the score for the pair term-doc
+                //add +1 for each docId I encounter throughout the posting list
+                documentQueryTermCount.put(docId, documentQueryTermCount.getOrDefault(docId, 0) + 1);
+
+                //get the score for the pair term-doc <=> the type is disjunctive
+                // or conjunctive and the doc counter is equal to the query terms
                 double termPartialScore = scoringFunction.documentWeight(term, posting);
                 //and sum it to the accumulator
                 documentScores.put(docId, documentScores.getOrDefault(docId, 0.0) + termPartialScore);
@@ -34,6 +39,12 @@ public class TAAT extends DocumentProcessor {
 
         //loop through the documentScore map
         for (Map.Entry<Integer, Double> entry : documentScores.entrySet()) {
+            if(queryType == QueryType.CONJUNCTIVE){
+                // if the count of query terms inside the doc is < of tot query terms we skip that doc
+                if(documentQueryTermCount.get(entry.getKey()) < queryTerms.size()){
+                    continue;
+                }
+            }
             priorityQueue.add(new DocumentScore(entry.getKey(), entry.getValue()));
             if (priorityQueue.size() > k) {
                 priorityQueue.pollLast();
