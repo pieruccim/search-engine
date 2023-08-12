995121d2bfa4569a6ed48fb52d3221af423a7616
Giacomo Pacini <giacomopacini98@gmail.com>
2023-08-12 09:41:34 +0200

    implemented the possibity to specify parameters as command line arguments

--- java/src/main/java/queryProcessing/QueryProcessorMain.java
+++ java/src/main/java/queryProcessing/QueryProcessorMain.java
@@ -10,7 +10,71 @@ import queryProcessing.QueryProcessor.DocumentProcessorType;
 
 public class QueryProcessorMain {
     public static void main(String[] args) {
-        QueryProcessor queryProcessor = new QueryProcessor(10, ScoringFunction.TFIDF, QueryType.DISJUNCTIVE, DocumentProcessorType.DAAT, true, true);
+
+        ScoringFunction scoringFunction = ScoringFunction.TFIDF;
+        QueryType queryType = QueryType.DISJUNCTIVE;
+        DocumentProcessorType documentProcessorType = DocumentProcessorType.DAAT;
+        int nResults = 10;
+        
+        for (int i = 0; i < args.length; i++) {
+            if(args[i].equals("--results") && (i+1) < args.length){
+                int tmp = Integer.parseInt(args[i+1]);
+                if(tmp > 0){    // && tmp < MAX_THRESHOLD
+                    nResults = tmp;
+                }else{
+                    System.out.println("The specified results number " + tmp + " is not valid, using default value " + nResults);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--scoring") && (i+1) < args.length){
+                ScoringFunction tmp = null;
+                try {
+                    tmp = ScoringFunction.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for scoring\tAccepted values: " + ScoringFunction.values().toString());
+                }
+                if(tmp != null){    // && tmp < MAX_THRESHOLD
+                    scoringFunction = tmp;
+                }else{
+                    System.out.println("The specified scoring function " + args[i+1] + " is not valid, using default value " + scoringFunction);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--queryType") && (i+1) < args.length){
+                QueryType tmp = null;
+                try {
+                    tmp = QueryType.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for QueryType\tAccepted values: " + QueryType.values().toString());
+                }
+                if(tmp != null){
+                    queryType = tmp;
+                }else{
+                    System.out.println("The specified Query type " + args[i+1] + " is not valid, using default value " + queryType);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--processingType") && (i+1) < args.length){
+                DocumentProcessorType tmp = null;
+                try {
+                    tmp = DocumentProcessorType.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for documentProcessorType\tAccepted values: " + DocumentProcessorType.values().toString());
+                }
+                if(tmp != null){
+                    documentProcessorType = tmp;
+                }else{
+                    System.out.println("The specified Processor type " + args[i+1] + " is not valid, using default value " + documentProcessorType);
+                }
+                i+=1;
+                continue;
+            }          
+        }
+
+        QueryProcessor queryProcessor = new QueryProcessor(nResults, scoringFunction, queryType, documentProcessorType, true, true);
 
         Scanner sc = new Scanner(System.in);
 
