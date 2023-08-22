2bdbf288b0412ad65ccaf5f49ea8d2a729bb579a
Enrico <nello.enrico@gmail.com>
2023-08-22 16:39:52 +0200

    first implementation of TrecEvalBenchmarks class for evaluating the benchmark queries and produce the results.tsv file

---
+++ java/src/main/java/queryPerformance/TrecEvalBenchmarks.java
@@ -0,0 +1,69 @@
+package queryPerformance;
+
+import common.manager.file.FileManager.*;
+import common.manager.file.TextualFileManager;
+import config.ConfigLoader;
+import javafx.util.Pair;
+import preprocessing.Preprocessor;
+import queryProcessing.DocumentProcessor.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrecEvalBenchmarks {
+
+    private static final String fixed = "Q0";
+    private static final String runId = "RUN-01";
+    private static final String queriesCharset = ConfigLoader.getProperty("performance.queries.charset");
+    private static final String queriesPath = ConfigLoader.getProperty("performance.queries.path");
+    private static final String resultsPath = ConfigLoader.getProperty("performance.results.path");
+    protected TextualFileManager queriesFileManager;
+    protected TextualFileManager resultsFileManager;
+
+    public TrecEvalBenchmarks(){
+        this.queriesFileManager = new TextualFileManager(queriesPath, MODE.READ, queriesCharset);
+        this.resultsFileManager = new TextualFileManager(resultsPath, MODE.WRITE);
+    }
+
+    /**
+     * The method is responsible for reading the queries from the benchmark file 'queries.dev.tsv'
+     * @return a list of pairs queryId-queryText
+     */
+    public List<Pair<Integer, String>> readQueries() {
+        List<Pair<Integer, String>> queries = new ArrayList<>();
+
+        String query;
+        while ((query = queriesFileManager.readLine()) != null) {
+            Pair<Integer, String> queryPair = Preprocessor.parseLine(query);
+            queries.add(queryPair);
+        }
+
+        return queries;
+    }
+
+    /**
+     * The method is responsible for writing on file each query read from the benchmark file along with its document score
+     * @param queryId is the first field in each row inside queries.dev.tsv file
+     * @param results is a list of DocumentScore elements containing the pair docId-score for the current query
+     */
+    protected void storeTrecEvalResult(int queryId, List<DocumentScore> results) {
+        if(results.size()==0){
+            return;
+        }
+
+        //writing results in trec_eval suitable format
+        for (DocumentScore documentScore : results) {
+            String resultLine = queryId + "\t" + fixed + "\t" + documentScore.getDocId() +
+                    "\t" + documentScore.getScore() + "\t" + runId;
+            resultsFileManager.writeLine(resultLine);
+        }
+    }
+
+    protected void closeResultFileManager(){
+        this.resultsFileManager.close();
+    }
+
+    protected void closeQueriesFileManager(){
+        this.queriesFileManager.close();
+    }
+}
---
+++ java/src/main/java/queryPerformance/TrecEvalBenchmarksMain.java
@@ -0,0 +1,109 @@
+package queryPerformance;
+
+import javafx.util.Pair;
+import preprocessing.Preprocessor;
+import queryProcessing.DocumentProcessor.*;
+import queryProcessing.QueryProcessor;
+import queryProcessing.QueryProcessor.*;
+
+import java.util.List;
+
+public class TrecEvalBenchmarksMain {
+    public static void main(String[] args) {
+
+        ScoringFunction scoringFunction = ScoringFunction.TFIDF;
+        QueryType queryType = QueryType.DISJUNCTIVE;
+        DocumentProcessorType documentProcessorType = DocumentProcessorType.DAAT;
+        int nResults = 10;
+
+        for (int i = 0; i < args.length; i++) {
+            if(args[i].equals("--results") && (i+1) < args.length){
+                int tmp = Integer.parseInt(args[i+1]);
+                if(tmp > 0){    // && tmp < MAX_THRESHOLD
+                    nResults = tmp;
+                }else{
+                    System.out.println("The specified results number " + tmp + " is not valid, using default value " + nResults);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--scoring") && (i+1) < args.length){
+                ScoringFunction tmp = null;
+                try {
+                    tmp = ScoringFunction.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for scoring\tAccepted values: " + ScoringFunction.values().toString());
+                }
+                if(tmp != null){    // && tmp < MAX_THRESHOLD
+                    scoringFunction = tmp;
+                }else{
+                    System.out.println("The specified scoring function " + args[i+1] + " is not valid, using default value " + scoringFunction);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--queryType") && (i+1) < args.length){
+                QueryType tmp = null;
+                try {
+                    tmp = QueryType.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for QueryType\tAccepted values: " + QueryType.values().toString());
+                }
+                if(tmp != null){
+                    queryType = tmp;
+                }else{
+                    System.out.println("The specified Query type " + args[i+1] + " is not valid, using default value " + queryType);
+                }
+                i+=1;
+                continue;
+            }
+            if(args[i].equals("--processingType") && (i+1) < args.length){
+                DocumentProcessorType tmp = null;
+                try {
+                    tmp = DocumentProcessorType.valueOf(args[i+1]);
+                } catch (IllegalArgumentException e) {
+                    //System.out.println("IllegalArgument for documentProcessorType\tAccepted values: " + DocumentProcessorType.values().toString());
+                }
+                if(tmp != null){
+                    documentProcessorType = tmp;
+                }else{
+                    System.out.println("The specified Processor type " + args[i+1] + " is not valid, using default value " + documentProcessorType);
+                }
+                i+=1;
+                continue;
+            }
+        }
+
+        QueryProcessor queryProcessor = new QueryProcessor(nResults, scoringFunction, queryType, documentProcessorType, true, true);
+        TrecEvalBenchmarks teb = new TrecEvalBenchmarks();
+
+        //process queries from the tsv trec_eval file
+        List<Pair<Integer, String>> queries = teb.readQueries();
+        int c = 0;
+        for (Pair<Integer, String> queryPair : queries) {
+            System.out.println("Evaluating Query n." + (c+1) + " ...");
+            int queryId = queryPair.getKey();
+            String queryText = queryPair.getValue();
+
+            List<DocumentScore> results = queryProcessor.processQuery(queryText);
+            if(results.size()>0){
+                teb.storeTrecEvalResult(queryId, results);
+            }else{
+                System.out.println("No relevant docs found for query "+(c+1));
+            }
+
+
+            c += 1;
+            /*if (c == 5) {
+                teb.closeQueriesFileManager();
+                teb.closeResultFileManager();
+                System.out.println("Done saving queries scores");
+                System.exit(0);
+            }*/
+        }
+
+        teb.closeQueriesFileManager();
+        teb.closeResultFileManager();
+        System.out.println("Done saving queries scores");
+    }
+}
