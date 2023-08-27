276ef113e03d508d6a472cb9ac3b2e5cbe6a3a23
Giacomo Pacini <giacomopacini98@gmail.com>
2023-08-27 17:51:19 +0200

    LRU Cache first implementation

---
+++ java/src/main/java/common/utils/LRUCache.java
@@ -0,0 +1,50 @@
+package common.utils;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+
+
+public class LRUCache<KeyType, ValueType> {
+
+    private int capacity;
+    private LinkedHashMap<KeyType,ValueType> map;
+
+    private Function<ValueType, Boolean> onRemotionCallable;
+
+    /**
+     * 
+     * @param capacity the number of objects that the cache can fit at most
+     * @param onRemotionCallable a callable that will be executed on item eviction from cache
+     */
+    public LRUCache(int capacity, Function<ValueType, Boolean> onRemotionCallable) {
+        this.capacity = capacity;
+        this.onRemotionCallable = onRemotionCallable;
+        this.map = new LinkedHashMap<>(16, 0.75f, true);
+    }
+
+    public ValueType get(KeyType key) {
+        ValueType value = this.map.get(key);
+        return value;
+    }
+
+    public void put(KeyType key, ValueType value) {
+        if (
+            !this.map.containsKey(key) &&
+            this.map.size() == this.capacity
+        ) {
+            Iterator<Map.Entry<KeyType,ValueType>> it = this.map.entrySet().iterator();
+            Entry<KeyType, ValueType> entry = it.next();
+            this.onRemotionCallable.apply(entry.getValue());
+            it.remove();
+        }
+        this.map.put(key, value);
+    }
+
+    public Collection<ValueType> values(){
+        return this.map.values();
+    }
+}
