9a45555effc4c8d18cdca99b3bd6aade9384751e
Giacomo Pacini <giacomopacini98@gmail.com>
2023-08-29 10:26:58 +0200

    concurrency handling

--- java/src/main/java/common/utils/LRUCache.java
+++ java/src/main/java/common/utils/LRUCache.java
@@ -1,6 +1,7 @@
 package common.utils;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -14,7 +15,7 @@ import config.ConfigLoader;
 public class LRUCache<KeyType, ValueType> {
 
     private int capacity;
-    private LinkedHashMap<KeyType,ValueType> map;
+    private Map<KeyType,ValueType> map;
 
     private final static boolean debug = ConfigLoader.getPropertyBool("cache.class.debug");
 
@@ -28,7 +29,7 @@ public class LRUCache<KeyType, ValueType> {
     public LRUCache(int capacity, Function<ValueType, Boolean> onRemotionCallable) {
         this.capacity = capacity;
         this.onRemotionCallable = onRemotionCallable;
-        this.map = new LinkedHashMap<>(16, 0.75f, true);
+        this.map = Collections.synchronizedMap( new LinkedHashMap<KeyType,ValueType>(16, 0.75f, true));
         
     }
     /**
@@ -36,7 +37,7 @@ public class LRUCache<KeyType, ValueType> {
      * @param key
      * @return
      */
-    public ValueType get(KeyType key) {
+    public synchronized ValueType get(KeyType key) {
         ValueType value = this.map.get(key);
         return value;
     }
@@ -45,28 +46,28 @@ public class LRUCache<KeyType, ValueType> {
 
     private static final int THRESHOLD = 10;
 
-    public void put(KeyType key, ValueType value) {
+    public synchronized void put(KeyType key, ValueType value) {
         if ( !this.map.containsKey(key) && this.map.size() >= this.capacity + THRESHOLD){
 
             if(mutex.tryAcquire()) {
 
-                Iterator<Map.Entry<KeyType,ValueType>> it = this.map.entrySet().iterator();
-                for (int i = 0; i < this.map.size() - this.capacity; i++) {
-                    
-                    Entry<KeyType, ValueType> entry = it.next();
-                    if(this.onRemotionCallable != null)
-                        this.onRemotionCallable.apply(entry.getValue());
-                    it.remove();
-                    if(debug) {
-                        System.out.println("removed key '"+ entry.getKey().toString() + "' from cache in order to add '" + key.toString() +"'");
-                        System.out.println("Was effectively removed? " + !this.map.containsKey(key));
-                        this.map.entrySet().iterator().forEachRemaining((Entry<KeyType, ValueType> f) -> {System.out.print(f.getKey().toString() + " ");});
-                        System.out.println();
-                    }
+                    Iterator<Map.Entry<KeyType,ValueType>> it = this.map.entrySet().iterator();
+                    for (int i = 0; i < this.map.size() - this.capacity; i++) {
+                        
+                        Entry<KeyType, ValueType> entry = it.next();
+                        if(this.onRemotionCallable != null)
+                            this.onRemotionCallable.apply(entry.getValue());
+                        it.remove();
+                        if(debug) {
+                            System.out.println("removed key '"+ entry.getKey().toString() + "' from cache in order to add '" + key.toString() +"'");
+                            System.out.println("Was effectively removed? " + !this.map.containsKey(key));
+                            this.map.entrySet().iterator().forEachRemaining((Entry<KeyType, ValueType> f) -> {System.out.print(f.getKey().toString() + " ");});
+                            System.out.println();
+                        }
 
-                }
-                // should close the iterator?
-                it = null;
+                    }
+                    
+                    it = null;
                 mutex.release();
             }
 
