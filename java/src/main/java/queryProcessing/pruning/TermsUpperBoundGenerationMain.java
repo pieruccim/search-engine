ff917e113440fbf397b4f044817b68d8c934bce0
Giacomo Pacini <giacomopacini98@gmail.com>
2023-08-29 16:41:16 +0200

    elapsed time information

--- java/src/main/java/queryProcessing/pruning/TermsUpperBoundGenerationMain.java
+++ java/src/main/java/queryProcessing/pruning/TermsUpperBoundGenerationMain.java
@@ -1,9 +1,17 @@
 package queryProcessing.pruning;
 
+import java.sql.Timestamp;
+
 import indexing.IndexerMain;
 
 public class TermsUpperBoundGenerationMain {
     public static void main(String[] args) {
+        long begin = System.currentTimeMillis();
+        System.out.println(new Timestamp(begin) + "\tStarting...");
         IndexerMain.upperBoundOnly(args);
+        long end = System.currentTimeMillis();
+        System.out.println(new Timestamp(end) + "\tFinished");
+        System.out.println("Elapsed time for terms upper bound generation: " + (end - begin));
+
     }
 }
