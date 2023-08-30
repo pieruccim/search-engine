a37d1f345cfa3f818f22a04d73bf5405a58eb46b
Giacomo Pacini <giacomopacini98@gmail.com>
2023-08-30 11:44:55 +0200

    fixed a bug in conjunctive logic of TAAT

--- java/src/main/java/queryProcessing/TAAT.java
+++ java/src/main/java/queryProcessing/TAAT.java
@@ -13,12 +13,20 @@ public class TAAT extends DocumentProcessor {
 
     private static TreeSet<DocumentScore> priorityQueue = new TreeSet<DocumentScore>(((Comparator<DocumentScore>)(DocumentScore::compare)).reversed());
     private static Map<Integer, Double> documentScores = new HashMap<>();
+    private static ArrayList<Integer> docIds = new ArrayList<Integer>();
+    private static ArrayList<Integer> docIdsNext = new ArrayList<Integer>();
+    private static ArrayList<Integer> swap;
 
     @Override
     public List<DocumentScore> scoreDocuments(List<VocabularyFileRecord> queryTerms, ScoreFunction scoringFunction, QueryType queryType, int k) {
 
         priorityQueue.clear();
         documentScores.clear();
+
+        if(queryType == QueryType.CONJUNCTIVE){
+            this.docIds.clear();
+            this.docIdsNext.clear();
+        }
         //Map<Integer, Integer> documentQueryTermCount = new HashMap<>(); // to check whether a doc contains all query terms
 
         boolean isFirstIterator = true;
@@ -39,14 +47,27 @@ public class TAAT extends DocumentProcessor {
                     //get the score for the pair term-doc <=> the type is disjunctive
                     // or conjunctive and the doc counter is equal to the query terms
                     documentScores.put(docId, scoringFunction.documentWeight(term, posting));
+
+                    if(queryType == QueryType.CONJUNCTIVE){
+                        docIds.add(docId);
+                    }
                 }else{
                     Double prevScore = documentScores.get(docId);
-                    if(queryType == QueryType.DISJUNCTIVE || prevScore != null)
+                    if(queryType == QueryType.DISJUNCTIVE || docIds.contains(docId)){
                         documentScores.put(docId, ((prevScore != null) ? prevScore: 0.0 ) + scoringFunction.documentWeight(term, posting) );
+                        if(queryType == QueryType.CONJUNCTIVE){
+                            docIdsNext.add(docId);
+                        }
+                    }
                 }
 
             }
-
+            if( ! isFirstIterator){
+                swap = docIds;
+                docIds = docIdsNext;
+                docIdsNext = swap;
+                docIdsNext.clear();
+            }
             isFirstIterator = false;
 
             //iterator.close();
